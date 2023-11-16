21556976358011de8488ee1f941e6f9f168548f1
Liv <110914989+livoszlak@users.noreply.github.com>
2023-11-16 10:47:24 +0100

    Working on divs with frames

---
+++ mainLiv.php
@@ -0,0 +1,242 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@700&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+
+<body>
+    <style>
+        svg {
+            width: 100%;
+            height: auto;
+        }
+
+        svg image {
+            object-fit: cover;
+        }
+
+        .wrapper {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: center;
+            gap: 45px;
+        }
+
+        body {
+            margin: 0 auto;
+            background-color: #0B0D0B;
+        }
+
+        .current,
+        .future {
+            display: flex;
+            flex-direction: column;
+            gap: 20px;
+            max-width: 1175px;
+        }
+
+        .headline {
+            font-family: 'Orbitron', monospace;
+            color: #04FCCF;
+            font-size: 32px;
+            text-align: center;
+            font-weight: 700;
+        }
+
+        .main {
+            display: flex;
+            flex-direction: column;
+            gap: 45px;
+            align-items: center;
+            padding-left: 120px;
+            padding-right: 120px;
+        }
+    </style>
+    <div class="main">
+        <div class="current">
+            <div class="headline">Nu på Retronautica</div>
+            <div class="wrapper">
+                <div class="poster">
+                    <svg width="260" height="380" viewBox="0 0 260 380" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <g clip-path="url(#clip0_425_764)">
+                            <rect x="11" y="11" width="238" height="358" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M51.5 0.5H67.5" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M81.5 0.5H124.5" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M145.5 0.5H205.5" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M259.5 150.5V227.5" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M205 379.5H245.5" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M0.5 181.5V216.5" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M0.5 232.5V248.5" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M0.5 346V379.5H91.5" stroke="#04FCCF" stroke-width="2" />
+                            <image x="11" y="11" width="238" height="358" href="assets/posters/bigAlien.jpeg" preserveAspectRatio="xMidYMid slice" />
+                        </g>
+                        <defs>
+                            <clipPath id="clip0_425_764">
+                                <rect width="260" height="380" fill="white" />
+                            </clipPath>
+                        </defs>
+                    </svg>
+                </div>
+                <div class="poster">
+                    <svg width="260" height="380" viewBox="0 0 260 380" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <g clip-path="url(#clip0_425_763)">
+                            <rect x="11" y="11" width="238" height="358" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M0.5 33V0.5H23" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M51.5 0.5H67.5" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M81.5 0.5H124.5" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M145.5 0.5H205.5" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M259.5 173.5V291.5" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M232.5 379.5H259.5H260" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M56.5 379.5H127" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M0.5 154.5V168.5" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M0.5 181.5V216.5" stroke="#04FCCF" stroke-width="2" />
+                            <image x="11" y="11" width="238" height="358" href="assets/posters/bigDune.jpg" preserveAspectRatio="xMidYMid slice" />
+                        </g>
+                        <defs>
+                            <clipPath id="clip0_425_763">
+                                <rect width="260" height="380" fill="white" />
+                            </clipPath>
+                        </defs>
+                    </svg>
+                </div>
+                <div class="poster">
+                    <svg width="260" height="380" viewBox="0 0 260 380" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <g clip-path="url(#clip0_425_766)">
+                            <rect x="11" y="11" width="238" height="358" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M0.5 82.5V126.5" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M0.5 195V326" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M259.5 99.5V156" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M157.5 379.5H256.5" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M239.5 0.5H219.5" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M203.5 0.5H170.5" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M0.5 46.5V73" stroke="#04FCCF" stroke-width="2" />
+                            <image x="11" y="11" width="238" height="358" href="assets/posters/bigWrathOfKhan.jpg" preserveAspectRatio="xMidYMid slice" />
+                        </g>
+                        <defs>
+                            <clipPath id="clip0_425_766">
+                                <rect width="260" height="380" fill="white" />
+                            </clipPath>
+                        </defs>
+                    </svg>
+
+                </div>
+                <div class="poster">
+                    <svg width="260" height="380" viewBox="0 0 260 380" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <g clip-path="url(#clip0_425_765)">
+                            <rect x="11" y="11" width="238" height="358" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M51.5 0.5H67.5" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M81.5 0.5H124.5" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M259.5 37.5V156" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M205 379.5H245.5" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M202 0.5H259.5V20.5" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M0.5 221.5V271" stroke="#04FCCF" stroke-width="2" />
+                            <image x="11" y="11" width="238" height="358" href="assets/posters/bigESB.jpeg" preserveAspectRatio="xMidYMid slice" />
+                        </g>
+                        <defs>
+                            <clipPath id="clip0_425_765">
+                                <rect width="260" height="380" fill="white" />
+                            </clipPath>
+                        </defs>
+                    </svg>
+
+                </div>
+            </div>
+        </div>
+        <div class="future">
+            <div class="headline">Snart på Retronautica</div>
+            <div class="wrapper">
+                <div class="poster">
+                    <svg width="260" height="380" viewBox="0 0 260 380" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <g clip-path="url(#clip0_425_763)">
+                            <rect x="11" y="11" width="238" height="358" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M0.5 33V0.5H23" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M51.5 0.5H67.5" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M81.5 0.5H124.5" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M145.5 0.5H205.5" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M259.5 173.5V291.5" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M232.5 379.5H259.5H260" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M56.5 379.5H127" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M0.5 154.5V168.5" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M0.5 181.5V216.5" stroke="#04FCCF" stroke-width="2" />
+                            <image x="11" y="11" width="238" height="358" href="assets/posters/bigTroopers.webp" preserveAspectRatio="xMidYMid slice" />
+                        </g>
+                        <defs>
+                            <clipPath id="clip0_425_763">
+                                <rect width="260" height="380" fill="white" />
+                            </clipPath>
+                        </defs>
+                    </svg>
+                </div>
+                <div class="poster">
+                    <svg width="260" height="380" viewBox="0 0 260 380" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <g clip-path="url(#clip0_425_766)">
+                            <rect x="11" y="11" width="238" height="358" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M0.5 82.5V126.5" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M0.5 195V326" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M259.5 99.5V156" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M157.5 379.5H256.5" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M239.5 0.5H219.5" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M203.5 0.5H170.5" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M0.5 46.5V73" stroke="#04FCCF" stroke-width="2" />
+                            <image x="11" y="11" width="238" height="358" href="assets/posters/bigDarkCity.jpg" preserveAspectRatio="xMidYMid slice" />
+                        </g>
+                        <defs>
+                            <clipPath id="clip0_425_766">
+                                <rect width="260" height="380" fill="white" />
+                            </clipPath>
+                        </defs>
+                    </svg>
+                </div>
+                <div class="poster">
+                    <svg width="260" height="380" viewBox="0 0 260 380" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <g clip-path="url(#clip0_425_764)">
+                            <rect x="11" y="11" width="238" height="358" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M51.5 0.5H67.5" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M81.5 0.5H124.5" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M145.5 0.5H205.5" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M259.5 150.5V227.5" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M205 379.5H245.5" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M0.5 181.5V216.5" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M0.5 232.5V248.5" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M0.5 346V379.5H91.5" stroke="#04FCCF" stroke-width="2" />
+                            <image x="11" y="11" width="238" height="358" href="assets/posters/bigApes.jpg" preserveAspectRatio="xMidYMid slice" />
+                        </g>
+                        <defs>
+                            <clipPath id="clip0_425_764">
+                                <rect width="260" height="380" fill="white" />
+                            </clipPath>
+                        </defs>
+                    </svg>
+                </div>
+                <div class="poster">
+                    <svg width="260" height="380" viewBox="0 0 260 380" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <g clip-path="url(#clip0_425_765)">
+                            <rect x="11" y="11" width="238" height="358" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M51.5 0.5H67.5" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M81.5 0.5H124.5" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M259.5 37.5V156" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M205 379.5H245.5" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M202 0.5H259.5V20.5" stroke="#04FCCF" stroke-width="2" />
+                            <path d="M0.5 221.5V271" stroke="#04FCCF" stroke-width="2" />
+                            <image x="11" y="11" width="238" height="358" href="assets/posters/bigBlade.webp" preserveAspectRatio="xMidYMid slice" />
+                        </g>
+                        <defs>
+                            <clipPath id="clip0_425_765">
+                                <rect width="260" height="380" fill="white" />
+                            </clipPath>
+                        </defs>
+                    </svg>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>

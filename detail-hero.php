2f1721c3b17fa50eded5fa5dcafdb311cd311791
Liv <110914989+livoszlak@users.noreply.github.com>
2023-12-11 18:28:59 +0100

    Changing hero images

---
+++ detail-hero.php
@@ -0,0 +1,21 @@
+<style>
+    .hero-img {
+        position: absolute;
+        transition: opacity 2s ease-in;
+        transition-delay: 1s;
+    }
+</style>
+<hero class="hero-detail-wrapper">
+    <div class="hero-img-wrapper">
+        <img class="hero-img" src="./assets/screencaps/2001-space-movie-screencaps 2.svg" alt="hero image" />
+        <img class="hero-img" src="./assets/screencaps/2001-space-movie-screencaps 3.svg" alt="hero image" />
+        <img class="hero-img" src="./assets/screencaps/2001-space-movie-screencaps 4.svg" alt="hero image" />
+        <img class="hero-img" src="./assets/screencaps/2001-space-movie-screencaps 5.svg" alt="hero image" />
+        <img class="hero-img" src="./assets/screencaps/2001-space-movie-screencaps 6.svg" alt="hero image" />
+        <img class="hero-img" src="./assets/screencaps/2001-space-movie-screencaps 7.svg" alt="hero image" />
+        <img class="hero-img" src="./assets/screencaps/2001-space-movie-screencaps 8.svg" alt="hero image" />
+        <img class="hero-img" src="./assets/screencaps/2001-space-movie-screencaps 9.svg" alt="hero image" />
+        <img class="hero-img" src="./assets/screencaps/2001-space-movie-screencaps 1.svg" alt="hero image" />
+    </div>
+</hero>
+<script src="detail-hero.js"></script>

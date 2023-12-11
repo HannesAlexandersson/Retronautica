8217c1b38d4bb87fdb123c16f0980e4f623284db
Liv <110914989+livoszlak@users.noreply.github.com>
2023-12-11 17:26:08 +0100

    More card work

--- main.php
+++ main.php
@@ -18,21 +18,33 @@
             <div class="poster first-frame">
                 <div class="poster-wrapper">
                     <img src="assets/posters/bigAlien.jpeg">
+                    <div class="button">
+                        <img src="assets/graphics/button-buy-black.svg">
+                    </div>
                 </div>
             </div>
             <div class="poster fourth-frame">
                 <div class="poster-wrapper">
                     <img src="assets/posters/bigDune.jpg">
+                    <div class="button">
+                        <img src="assets/graphics/button-buy-black.svg">
+                    </div>
                 </div>
             </div>
             <div class="poster second-frame">
                 <div class="poster-wrapper">
                     <img src="assets/posters/bigWrathOfKhan.jpg">
+                    <div class="button">
+                        <img src="assets/graphics/button-buy-black.svg">
+                    </div>
                 </div>
             </div>
             <div class="poster third-frame">
                 <div class="poster-wrapper">
                     <img src="assets/posters/bigESB.jpeg">
+                    <div class="button">
+                        <img src="assets/graphics/button-buy-black.svg">
+                    </div>
                 </div>
             </div>
         </div>
@@ -46,21 +58,33 @@
             <div class="poster first-frame">
                 <div class="poster-wrapper">
                     <img src="assets/posters/bigTroopers.webp">
+                    <div class="button">
+                        <img src="assets/graphics/button-info-black.svg">
+                    </div>
                 </div>
             </div>
             <div class="poster second-frame">
                 <div class="poster-wrapper">
                     <img src="assets/posters/bigDarkCity.jpg">
+                    <div class="button">
+                        <img src="assets/graphics/button-info-black.svg">
+                    </div>
                 </div>
             </div>
             <div class="poster fourth-frame">
                 <div class="poster-wrapper">
                     <img src="assets/posters/bigApes.jpg">
+                    <div class="button">
+                        <img src="assets/graphics/button-info-black.svg">
+                    </div>
                 </div>
             </div>
             <div class="poster third-frame">
                 <div class="poster-wrapper">
                     <img src="assets/posters/bigBlade.webp">
+                    <div class="button">
+                        <img src="assets/graphics/button-info-black.svg">
+                    </div>
                 </div>
             </div>
         </div>
